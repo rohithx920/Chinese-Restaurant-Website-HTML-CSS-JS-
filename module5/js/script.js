0ba6cdca901ed65954c023bd8e0aef3040990b2f
rohithx920 <rohithsagar.14u@gmail.com>
2016-10-06 00:02:09 -0400

    update 3

--- module5/js/script.js
+++ module5/js/script.js
@@ -25,26 +25,39 @@ $(function(){
 });
 
 (function (global) {
-	var dc={};
-	var homehtml="snippets/home-snippet.html";
-	var insertHtml=function(selector, html){
-		var targetElem=document.qureySelector(selector);
-		targetElem.innerHTML=html;
-	}
-	var showLoading= function(selector){
-		var html="<div class='text-center'>";
-		html +="<img src='images/ajax-loader.gif'></div>"
-		insertHtml(selector,html);
-	}
 
-	//On page load(before images or css)
-	document.addEventListener("DOMContentLoaded", function(event){
-		showLoading("#main-content");
-		$ajaxUtils.sendGetRequest(
-			homehtml,function(responseText){
-				document.qureySelector("#main-content")
-				.innerHTML=responseText;
-			}, false);
-	});
+var dc = {};
 
+var homeHtml = "snippets/home-snippet.html";
+
+// Convenience function for inserting innerHTML for 'select'
+var insertHtml = function (selector, html) {
+  var targetElem = document.querySelector(selector);
+  targetElem.innerHTML = html;
+};
+
+// Show loading icon inside element identified by 'selector'.
+var showLoading = function (selector) {
+  var html = "<div class='text-center'>";
+  html += "<img src='images/ajax-loader.gif'></div>";
+  insertHtml(selector, html);
+};
+
+// On page load (before images or CSS)
+document.addEventListener("DOMContentLoaded", function (event) {
+
+// On first load, show home view
+showLoading("#main-content");
+$ajaxUtils.sendGetRequest(
+  homeHtml,
+  function (responseText) {
+    document.querySelector("#main-content")
+      .innerHTML = responseText;
+  },
+  false);
 });
+
+
+global.$dc = dc;
+
+})(window);
